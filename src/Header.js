5686e0f1bc7081427eff97f58ae58a0570577c17
alexanderajju <alexanderajju@gmail.com>
2020-07-14 08:20:31 +0530

    search box

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import './Header.css';
-import {Link } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
-    return (
-        <Link to="/">
-        <nav className="header">
-            <img
-            className="header__logo"
-             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
-        </nav>
-        
-        </Link>
-        
-    )
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+
+
+
+
+          
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
