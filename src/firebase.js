3c5d7b17b9026ac521cbbaaa57bbf0cde90a9427
alexanderajju <alexanderajju@gmail.com>
2020-07-14 07:07:38 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAaARs7ZFBsJjyT4QpUJZS_D16QflGnjTc",
+    authDomain: "clone-f5432.firebaseapp.com",
+    databaseURL: "https://clone-f5432.firebaseio.com",
+    projectId: "clone-f5432",
+    storageBucket: "clone-f5432.appspot.com",
+    messagingSenderId: "431519560344",
+    appId: "1:431519560344:web:c139a19c564975da6916ed",
+    measurementId: "G-MN14ZHTCYX"
+  };
