96460e7f26cb0334aeec16de72754199d74de951
alexanderajju <alexanderajju@gmail.com>
2020-07-14 12:30:03 +0530

    added checkoutproduct componet

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,10 +12,22 @@ function Checkout() {
              {basket?.length ===0 ? (
                  <div>
                      <h2>Your Shopping Basket is empty</h2>
+                     <p>
+                         you have no items in your basket. To buy one or more items, click 
+                         "Add to basket" next to the item.
+                     </p>
                  </div>
              ):(
                  <div>
-                     <h2>Your Shopping Basket</h2>
+                     <h2 className="checkout__title">Your Shopping Basket</h2>
+                     {basket.map((item)=>(
+                         <CheckoutProduct
+                         item={item.id}
+                         title={item.title}
+                         image={item.image}
+                         price={item.image}
+                         rating={item.rating}/>
+                     ))}
                  </div>
              )}
         </div>
