c0b5fb8479a3fa36582f3f4f243f39b77be8f500
alexanderajju <alexanderajju@gmail.com>
2020-07-14 09:12:12 +0530

    home component added with image

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/M21/NewKvs/D15477737_Samsung_M21_Teaser_TakeOver_DesktopTallHero_1500x600_3._CB410497324_.jpg" alt=""/>
+
+        </div>
+    )
+}
+
+export default Home
