c762cf9f28dd0e34987d9fb009d85a0dcd38b5bd
alexanderajju <alexanderajju@gmail.com>
2020-07-14 12:21:48 +0530

    Baket text added

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Checkout.css';
+import { useStateValue } from './StateProvider';
 
 function Checkout() {
+    const [{basket}]=useStateValue();
     return (
         <div className="checkout">
-            <h1>  from Checkout</h1>
+            <img
+            className="checkout__add"
+             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
+             {basket?.length ===0 ? (
+                 <div>
+                     <h2>Your Shopping Basket is empty</h2>
+                 </div>
+             ):(
+                 <div>
+                     <h2>Your Shopping Basket</h2>
+                 </div>
+             )}
         </div>
     )
 }
